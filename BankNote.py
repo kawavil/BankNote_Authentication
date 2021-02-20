88051e77c7f749e54dbae0a99719a4a0fa197c39
Vilas Kaware <vilaskaware1234@gmail.com>
2021-02-20 23:11:38 +0530

    adding requirement and procfile

--- BankNote.py
+++ BankNote.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 # pydantic provides some features like error reporting and data handling(reporting)
 
+
 class BankNote(BaseModel):
     variance: float
     skewness: float
